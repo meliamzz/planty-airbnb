981bbf986fe5126e939f7aa1e560c248bedc582e
Melia Mazouzi <melia.mazouzi@gmail.com>
2021-11-22 18:37:38 +0100

    All must have routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :plants, only: [:index, :show] do
+    resources :bookings_requests, only: [:create]
+  end
+  resources :my_plants, except: [:show]
+  resources :bookings, only: [:index] do
+    member do
+      patch :accept
+      patch :decline
+    end
+  end
+  resources :bookings_requests, only: [:index]
 end
