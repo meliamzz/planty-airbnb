28f848bb26a4b6d60567194eb4effcfb747c51da
Melia Mazouzi <melia.mazouzi@gmail.com>
2021-11-24 11:24:20 +0100

    updating master branch

---
+++ app/controllers/bookings_requests_controller.rb
@@ -0,0 +1,5 @@
+class BookingsRequestsController < ApplicationController
+  def create
+
+  end
+end
---
+++ test/controllers/bookings_requests_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BookingsRequestsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
