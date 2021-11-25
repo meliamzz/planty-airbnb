142f2d3493a767c01906f6296d855c251c902820
nicolas jolys <jolys.nicolas@gmail.com>
2021-11-25 16:02:57 +0100

    index my_plants

--- app/controllers/owner/my_plants_controller.rb
+++ app/controllers/owner/my_plants_controller.rb
@@ -1,7 +1,18 @@
 class Owner::MyPlantsController < ApplicationController
+    before_action :set_plant, only: [ :show ]
+
   def index
     @user = current_user
-    
 
+
+  end
+
+  def show
+  end
+
+  private
+
+  def set_plant
+    @plant = Plant.find(params[:id])
   end
 end
